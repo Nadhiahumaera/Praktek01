0293291b94f5a4ca18c65f27eba47d16b629ecfd
nadhiahumaera <nadhiahumaera0@gmail.com>
2023-05-27 14:15:11 +0700

    Mengisi nilai atribut

--- src/praktek01/MahasiswaAksi.java
+++ src/praktek01/MahasiswaAksi.java
@@ -13,6 +13,11 @@ public class MahasiswaAksi {
     public static void main(String[] args) {
         Mahasiswa Harry = new Mahasiswa();
         
+        Harry.nama="Harry Potter";
+        Harry.NPM="19639999";
+        Harry.IPK=3.4;
+        
+        
         }
     
     }
